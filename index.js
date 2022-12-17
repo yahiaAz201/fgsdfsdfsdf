144def832220b6ab3e61b5dbeef50e717d9a7ec2
yahia <yahia.bourouni.79@gmail.com>
2022-12-17 15:14:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+const port = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  const response = await axios.get(
+    "https://sso.godaddy.com/?realm=pass&app=o365",
+    {
+      headers: {
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+      },
+    }
+  );
+  res.send(response.data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
